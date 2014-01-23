385725cb3f82e882f42e0b685b99a3321669c022
Brandon <skinty@gmail.com>
2014-01-23 11:32:09 +0200

    Renamed controllers to conform to naming convention Some UI tweaks and additions to Actions dropdown Added notification icon per miner Added Pools nav button

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,32 +1,36 @@
 'use strict';
 
 angular.module('nodeminerApp', [
-  'ngCookies',
-  'ngResource',
-  'ngSanitize',
-  'ngRoute'
-])
-  .config(function ($routeProvider, $locationProvider) {
-    $routeProvider
-      .when('/miners', {
-        templateUrl: 'partials/settings',
-        controller: 'MainCtrl'
-      })
-      .when('/settings', {
-        templateUrl: 'partials/settings',
-        controller: 'MainCtrl'
-      })
-      .when('/coins', {
-        templateUrl: 'partials/coins',
-        controller: 'MainCtrl'
-      })
-      .when('/', {
-        templateUrl: 'partials/main',
-        controller: 'MainCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-      
-    $locationProvider.html5Mode(true);
-  });
+        'ngCookies',
+        'ngResource',
+        'ngSanitize',
+        'ngRoute'
+    ])
+    .config(function ($routeProvider, $locationProvider) {
+        $routeProvider
+            .when('/miners', {
+                templateUrl: 'partials/settings',
+                controller: 'MainCtrl'
+            })
+            .when('/settings', {
+                templateUrl: 'partials/settings',
+                controller: 'MainCtrl'
+            })
+            .when('/coins', {
+                templateUrl: 'partials/coins',
+                controller: 'MainCtrl'
+            })
+            .when('/pools', {
+                templateUrl: 'partials/coins',
+                controller: 'MainCtrl'
+            })
+            .when('/', {
+                templateUrl: 'partials/main',
+                controller: 'MainCtrl'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+
+        $locationProvider.html5Mode(true);
+    });
