8fa9a4e478078cd1c695e96c96c8ea93819d8466
Brandon Barker <skinty@gmail.com>
2014-02-18 23:48:15 +0200

    Removed Google Fonts references Added popover confirmations to all delete functions

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -28,4 +28,51 @@ angular.module('nodeminerApp', [
       });
 
     $locationProvider.html5Mode(true);
+  })
+  .directive('ngPopoverConfirm', function () {
+    return {
+      restirct: 'A',
+      link: function (scope, element, attrs) {
+        console.log(attrs);
+        element.popover({
+          html: attrs['html'] || true,
+          placement: attrs['placement'] || 'right',
+          trigger: attrs['trigger'] || 'manual',
+          content: attrs['content'] || "<span data-popover-confirm-container> \
+          <span class='btn btn-success btn-sm popover-confirm-btn'> \
+            <i class='fa fa-check fa-fw'></i>&nbsp; Yes \
+          </span> \
+          <span class='btn btn-danger btn-sm popover-cancel-btn'> \
+            <i class='fa fa-times fa-fw'></i>&nbsp; No \
+          </span> \
+        </span>"
+        });
+
+        $(document).off('click', '[ng-popover-confirm]');
+        $(document).on('click', '[ng-popover-confirm]', function () {
+          var btn = $(this),
+            popover = btn.next();
+
+          btn.popover('toggle');
+        });
+
+        $(document).off('click', '.popover-confirm-btn');
+        $(document).on('click', '.popover-confirm-btn', function () {
+          var popover = $(this).closest('.popover'),
+            btn = popover.prev(),
+            callback = btn.attr('ng-popover-confirm'),
+            result = eval('scope.' + callback);
+
+          btn.popover('hide');
+        });
+
+        $(document).off('click', '.popover-cancel-btn');
+        $(document).on('click', '.popover-cancel-btn', function () {
+          var popover = $(this).closest('.popover'),
+            btn = popover.prev();
+
+          btn.popover('hide');
+        });
+      }
+    }
   });
